cb7c6a13838679ec90ce36f0ddf37bdfd1c30076
Vere_ia36 <Vere_ia36@student.itstep.org>
2023-04-08 23:00:40 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './components/chat/user-list/user-list.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 const routes: Routes = [
-  { path: '', component: UserListComponent },
+  { path: '', component: HomeComponent },
   { path: 'chat/:id', component:  ChatComponent},
+  { path: 'login', component:  LoginComponent},
+  { path: 'register', component:  RegisterComponent},
+  { path: 'users', component:  UserListComponent},
 
 ];
 
